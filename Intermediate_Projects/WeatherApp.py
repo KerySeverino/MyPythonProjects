d06de4af5af0a0be3e5256ebedc8fdfb55cb88cc
KerySeverino <KerySeverino@gmail.com>
2023-10-09 17:25:56 -0400

    Improvements over the first version: This version utilizes ttkBootstrap instead of tk, enhancing the customization and modern appearance of the widgets and GUI. It also introduces weather search functionality, allowing users to input a city and initiate the search by clicking the (Enter) button. I will try to improve it later on into an Advanced Project.

--- Intermediate_Projects/WeatherApp.py
+++ Intermediate_Projects/WeatherApp.py
@@ -1,12 +1,50 @@
-import tkinter as tk
-#(ttk) has more modern GUI looks when compare to tK
-from tkinter import ttk 
-#You will need to install (requests) with your terminal (Mac: pip3 install requests / Window: pip install request)
-import requests
+import ttkbootstrap as ttk # You will need to download this, Mac: pip3 install ttkbootstrap, Window: pip install ttkbootstrap
+import requests # You will need to download this, Mac: pip3 install requests, Window: pip install requests
 import os
 
-def city_Input():
-    CITY = entry_Str.get()
+#API from OpenWeatherMap: Gets the weather data to display it
+def fetch_weather_data(event=None):
+    CITY = entry.get()
+    BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid="
+    URL = BASE_URL + api_key + "&q=" + CITY
+    response = requests.get(URL).json()
+    update_display(response)
+
+#Updates the display every time the user enters city 
+def update_display(response):
+    temperature_label.config(text="TEMPERATURE: " + str(get_Fahrenheit(response)) + "°F")
+    feels_like_label.config(text="FEELS LIKE: " + str(get_Feels_Like(response)) + "°F")
+    humidity_label.config(text="HUMIDITY: " + str(get_Humidity(response)) + "%")
+    visibility_label.config(text="VISIBILITY: " + str(get_Visibility(response)) + " mi")
+    windSpeed_label.config(text="WINDSPEED: " + str(get_windSpeed(response)) + " MPH")
+
+#Turns kelvin to Fahrenheit, and gets the temp
+def get_Fahrenheit(response):
+    kelvin_Temp = response["main"]["temp"]
+    fahrenheit_Temp = (kelvin_Temp - 273.15) * 9/5 + 32
+    return int(fahrenheit_Temp)
+
+#Turns kelvin to Fahrenheit, and gets the feels like
+def get_Feels_Like(response):
+    kelvin_Temp = response["main"]["feels_like"]
+    fahrenheit_Temp = (kelvin_Temp - 273.15) * 9/5 + 32
+    return int(fahrenheit_Temp)
+
+#Gets the humitity
+def get_Humidity(response):
+    humidity = response["main"]["humidity"]
+    return int(humidity)
+
+#Gets the visibility, by calculating meters, kilometers and miles
+def get_Visibility(response):
+    meters_visibility = response["visibility"]
+    kilometers_visibility = meters_visibility * 0.001
+    miles = kilometers_visibility * 0.621371
+    return int(miles)
+
+def get_windSpeed(response):
+    windSpeed = response["wind"]["speed"]
+    return int(windSpeed)
 
 # Define the path to the API key file using a relative path
 api_key_file = os.path.expanduser("~/Desktop/MyProjects/weather_api_Key.txt")
@@ -15,46 +53,63 @@ api_key_file = os.path.expanduser("~/Desktop/MyProjects/weather_api_Key.txt")
 with open(api_key_file, "r") as file:
     api_key = file.read()
 
-CITY = "New York, US"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid="
-URL = BASE_URL + api_key + "&q=" + CITY
-response = requests.get(URL).json()
-#Debugging API responsed
-#print(response)
+#Creates the window and sets the theme using ttKBootstrap 
+window = ttk.Window(themename = "darkly")
 
-
-#Window
-window = tk.Tk()
 window.title("KersevWeather")
-window.geometry("300x150")
+window.geometry("400x300")
 
-#Widgets for the app
-#Title
+
+# Title
 title_Label = ttk.Label(master = window, text = "KersevWeather", font = "Arial 24 bold")
 title_Label.pack()
 
-#User instructions
+# User instructions
 instruction_Label = ttk.Label(master = window, text = "Please enter a city", font = "Arial 12 bold")
-instruction_Label.pack()
+instruction_Label.pack(pady = 10)
 
 #####################################################################################################
-#Input field -> This allows me to combine the entry user input field and the Enter button into a single unit within the Input Frame.
+# Input field
 input_Frame = ttk.Frame(master = window)
 
-#Entry user input
-entry_Str = ttk.Entry(master = input_Frame)
-entry_Str.pack(side = "left", padx = 10)
+entry_Str = ttk.StringVar()
+entry = ttk.Entry(master = input_Frame, textvariable = entry_Str)
+entry.pack(side ="left", pady = 10)
 
-#Enter Button for the city input
-enter_Button = ttk.Button(master = input_Frame, text= "Enter", command = city_Input)
-enter_Button.pack(side = "right", padx = 10)
-input_Frame.pack(pady = 10)
+# Fetch Weather button
+enter_button = ttk.Button(master=input_Frame, text="Enter", padding = (20,10),command = fetch_weather_data)
+enter_button.pack(side = "right", padx = 10)
+
+input_Frame.pack()
 #####################################################################################################
 
-#Output 
-output_Label = ttk.Label(master = window, text = "Output", font = "Arial 16")
-output_Label.pack()
 
-#Runner
+#####################################################################################################
+# Output field
+output_Label = ttk.Label(master = window)
+temperature_label = ttk.Label(master = output_Label, text = "", font="Arial 12 bold")
+feels_like_label = ttk.Label(master = output_Label, text = "", font="Arial 12 bold")
+humidity_label = ttk.Label(master = output_Label, text = "", font="Arial 12 bold")
+visibility_label = ttk.Label(master = output_Label, text = "", font="Arial 12 bold")
+windSpeed_label = ttk.Label(master = output_Label, text = "", font="Arial 12 bold")
+
+temperature_label.pack(side = "top")
+feels_like_label.pack(side = "top")
+humidity_label.pack(side = "top")
+visibility_label.pack(side = "top")
+windSpeed_label.pack(side = "top")
+
+output_Label.pack()
+#####################################################################################################
+
+# Starts the Tkinter main loop
+
+#Debugging API calls
+# CITY = "New York"
+# BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid="
+# URL = BASE_URL + api_key + "&q=" + CITY
+# response = requests.get(URL).json()
+# print(response)
+
 window.mainloop()
 

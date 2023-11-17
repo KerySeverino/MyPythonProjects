893962a7bb523ec439bce436594bcb9986c9ab40
KerySeverino <KerySeverino@gmail.com>
2023-11-17 04:52:30 -0500

    I added the ability for players to choose whether they want to play again. I also implemented a color change when the mouse is hovering over the button. Additionally, I included rules or instructions in the play-again menu.

--- Beginner_Projects/tic_tac_toe.py
+++ Beginner_Projects/tic_tac_toe.py
@@ -4,10 +4,11 @@ import pygame
 Steps needed to created the game:
 
 1. Draw the canvas/screen
-2. Created a Grid for the game
-3. Check for action: Left click = O, Right click = X
-4. Check if: Win, Lost, Tie
-5. Check if player wants to: Play again
+2. Create a Grid for the game
+3. Create players
+4. Check for action: Left click = O, Right click = X
+5. Check if: Win, Lost, Tie
+6. Check if player wants to: Play again
 """
 
 pygame.init()
@@ -18,17 +19,23 @@ GRID_SIZE = 3
 CELL_SIZE = WIDTH // GRID_SIZE
 
 # Colors
-background_color = (255, 253, 208)
+BACKGROUND_COLOR = (255, 253, 208)
 LINE_COLOR = (0, 0, 0)
+HOVER_COLOR = (185, 177, 167)
 
 # Display
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Tic-Tac-Toe")
 quit = False #Pressing (red - X) on top-right of screen
-board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  # 0 = Empty, 1 = X / Left-click, 3 = O / Right-click
+
+# 0 = Empty, 1 = X / Left-click, 3 = O / Right-click
+board = [[0, 0, 0], 
+         [0, 0, 0], 
+         [0, 0, 0]]  
 
 # Font
 font = pygame.font.SysFont(None, 100)
+rules_font =  pygame.font.SysFont(None, 30)
 
 def draw_grid():
     # Drawing horizontal Lines
@@ -44,11 +51,13 @@ def draw_player():
             if board[row][column] == 1:  # X / Left-click
                 x_pos = column * CELL_SIZE + CELL_SIZE // 4
                 y_pos = row * CELL_SIZE + CELL_SIZE // 4
+
                 pygame.draw.line(screen, LINE_COLOR, (x_pos, y_pos), (x_pos + CELL_SIZE // 2, y_pos + CELL_SIZE // 2), 5)
                 pygame.draw.line(screen, LINE_COLOR, (x_pos, y_pos + CELL_SIZE // 2), (x_pos + CELL_SIZE // 2, y_pos), 5)
             elif board[row][column] == 3:  # O / Right-click
                 x_pos = column * CELL_SIZE + CELL_SIZE // 4
                 y_pos = row * CELL_SIZE + CELL_SIZE // 4
+
                 pygame.draw.circle(screen, LINE_COLOR, (x_pos + CELL_SIZE // 4, y_pos + CELL_SIZE // 4), CELL_SIZE // 4, 5)
 
 def check_winner():
@@ -91,13 +100,27 @@ def check_tie():
                 return False  # There is an empty space, game is not a tie
     return True  # All spaces are filled, game is a tie
 
+#Play again / Result Menu
 def display_result(result):
     text = font.render(result, True, LINE_COLOR)
-    screen.blit(text, (WIDTH // 4, HEIGHT // 4))
+    screen.blit(text, (WIDTH // 4, HEIGHT // 3))
 
+def display_playagain_text(text):
+    text = font.render(text, True, LINE_COLOR)
+    screen.blit(text, (WIDTH // 6, HEIGHT // 2))
+
+def display_playagain_border(hover):
+    pygame.draw.rect(screen, hover, pygame.Rect(WIDTH // 6, HEIGHT // 2, 390, 70))
+    
+def game_rules(text):
+    text = rules_font.render(text, True, LINE_COLOR)
+    screen.blit(text, (WIDTH // 8, HEIGHT // 6))
+    
 # Game loop
 while not quit:
-    screen.fill(background_color)
+    screen.fill(BACKGROUND_COLOR)
+    play_again_rect = pygame.Rect(WIDTH // 6, HEIGHT // 2, 390, 50)
+    start_rect = pygame.Rect(WIDTH // 3, HEIGHT // 4, 180, 70)
 
     # Event handler
     for event in pygame.event.get():
@@ -113,20 +136,41 @@ while not quit:
             elif event.button == 3 and board[clicked_row][clicked_col] == 0:  # Right click
                 board[clicked_row][clicked_col] = 3
 
-    # Drawing the play area / grid
+            #Result menu
+            if check_winner() or check_tie():
+                if event.button == 1 and play_again_rect.collidepoint(mouseX, mouseY):
+                    board = [[0, 0, 0], 
+                             [0, 0, 0], 
+                             [0, 0, 0]]
+
+    #Color changes when mouse is near it
+    hover_color = HOVER_COLOR if play_again_rect.collidepoint(pygame.mouse.get_pos()) else BACKGROUND_COLOR
+ 
+    screen.fill(BACKGROUND_COLOR)
     draw_grid()
     draw_player()
-
+       
     # Check for game result
     if check_winner() == 1:
-        screen.fill((background_color))
-        display_result("X wins!")
+            screen.fill((BACKGROUND_COLOR))
+            display_result("X wins !")
+            display_playagain_border(hover_color)
+            display_playagain_text("Play again?")
+            game_rules("Rules: A line horizontally, vertically, or diagonally")
+                
     elif check_winner() == 3:
-        screen.fill((background_color))
-        display_result("O wins!")
+            screen.fill((BACKGROUND_COLOR))
+            display_result("O wins !")
+            display_playagain_border(hover_color)
+            display_playagain_text("Play again?")
+            game_rules("Rules: A line horizontally, vertically, or diagonally")
+
     elif check_tie():
-        screen.fill((background_color))
-        display_result("It's a tie!")
+            screen.fill((BACKGROUND_COLOR))
+            display_result("It's a tie!")
+            display_playagain_border(hover_color)
+            display_playagain_text("Play again?")
+            game_rules("Rules: A line horizontally, vertically, or diagonally")
 
     # Update game state
     pygame.display.update()

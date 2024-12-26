0a51c27ef2230893bca2813b81b6ac281bd9e9cb
Kery <Keryseverino@gmail.com>
2024-12-26 13:30:58 -0500

    Changing file locations

--- Beginner_Projects/CountDown.py
+++ Beginner_Projects/CountDown.py
@@ -21,18 +21,18 @@ label3 = Button(window, padx=20 , pady=20).grid()
 #Executes Tkinder
 window.mainloop()
 
-# my_timer = int(input("Please enter the time in seconds: "))
-# my_date = datetime.datetime.now()
+my_timer = int(input("Please enter the time in seconds: "))
+my_date = datetime.datetime.now()
 
-# #range(my_timer, 0, -1) reverses the countDown, so it starts from the highest time first and counts down
-# for countDown in range(my_timer, 0, -1):
-#     seconds = countDown % 60
-#     minutes = int(countDown / 60) % 60
-#     hours = int(countDown / 3600)
-#     #seconds:02 - 02 basically adds 0 as padding, every if the digit is not 2
-#     print(f"{hours:02}:{minutes:02}:{seconds:02}")
+#range(my_timer, 0, -1) reverses the countDown, so it starts from the highest time first and counts down
+for countDown in range(my_timer, 0, -1):
+    seconds = countDown % 60
+    minutes = int(countDown / 60) % 60
+    hours = int(countDown / 3600)
+    #seconds:02 - 02 basically adds 0 as padding, every if the digit is not 2
+    print(f"{hours:02}:{minutes:02}:{seconds:02}")
 
-#     #Makes the countdown wait 1 second before printing
-#     time.sleep(1)
+    #Makes the countdown wait 1 second before printing
+    time.sleep(1)
 
-#     print("CLOCK: TIME'S UP!!!"+ " - " + my_date.strftime("%c"))
+    print("CLOCK: TIME'S UP!!!"+ " - " + my_date.strftime("%c"))
--- Beginner_Projects/ai_tic_tac_toe.py
+++ Beginner_Projects/ai_tic_tac_toe.py
@@ -22,26 +22,42 @@ def main():
 
     WIDTH, HEIGHT = 800, 800
     FPS = 60
+    GRID_SIZE = 3
+    CELL_SIZE = WIDTH // GRID_SIZE
+    LINE_COLOR = (255, 215, 0)
+
     clock = pygame.time.Clock()
 
     pygame.display.set_caption("Tic-Tac-Toe")
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    image_path = os.path.join("tictactoe_assets", "tictactoe_background_img.jpg")
+    image_path = os.path.join("Beginner_Projects", "tictactoe_assets", "blackframe.jpg")
     BG_IMG = pygame.transform.scale(pygame.image.load(image_path), (WIDTH, HEIGHT))
 
+
+    def draw_grid():
+        # Drawing horizontal Lines
+        for i in range(1, GRID_SIZE):
+            pygame.draw.line(screen, LINE_COLOR, (90, i * CELL_SIZE), (710, i * CELL_SIZE), 3)
+        # Drawing vertical lines
+        for i in range(1, GRID_SIZE):
+            pygame.draw.line(screen, LINE_COLOR, (i * CELL_SIZE, 90), (i * CELL_SIZE, 710), 3)
+
     def redraw_window(screen, bg_img):
-        screen.blit(bg_img, (0,0))
-        
+        screen.blit(bg_img, (0, 0))
+        draw_grid()
+
+        pygame.display.update()
+
 
     running = True
     while running:
-
-        clock.ticks(FPS)
+        clock.tick(FPS)
         redraw_window(screen, BG_IMG)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                pygame.quit()
+
+    pygame.quit()
     
 main()
